5433011882f8cb80d51070bbeb7fc23dcfffd463
JosefinaLucarelli <joulucarelli@gmail.com>
2023-04-21 16:13:50 -0300

    install and create examplecomponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from '@chakra-ui/react'
+
 const App = () => {
   return (
-    <h1>HELLO WORLD</h1>
-   )
+    <StatGroup>
+      <Stat>
+        <StatLabel>Sent</StatLabel>
+        <StatNumber>345,670</StatNumber>
+        <StatHelpText>
+          <StatArrow type='increase' />
+          23.36%
+        </StatHelpText>
+      </Stat>
+
+      <Stat>
+        <StatLabel>Clicked</StatLabel>
+        <StatNumber>45</StatNumber>
+        <StatHelpText>
+          <StatArrow type='decrease' />
+          9.05%
+        </StatHelpText>
+      </Stat>
+    </StatGroup>
+  )
 }
 
 export default App;
